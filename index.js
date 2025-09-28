68d1128b70fdc8f2213f1890367b6d488417aaf0
VaibhavDev-29 <rohit9875756@gmail.com>
2025-09-28 16:40:16 +0530

    add click and touch functionalit

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ function removeTransition(e) {
   e.target.classList.remove("playing");
 }
 
-
-
-function playsound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+function playSoundByKeyCode(keyCode) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${keyCode}"]`);
   if (!audio) return;
 
   key.classList.add("playing");
@@ -15,10 +13,38 @@ function playsound(e) {
   audio.play();
 }
 
+function playsound(e) {
+  // Keyboard event
+  playSoundByKeyCode(e.keyCode);
+}
+
+function handleClickOrTouch(e) {
+  // Get data-key attribute of clicked element
+  const keyElement = e.target.closest(".key");
+  if (!keyElement) return; // if not clicked on a key
+
+  const keyCode = keyElement.getAttribute("data-key")
+  playSoundByKeyCode(keyCode);
+}
+
+
+
+
+
 // setTimeout(() => {
 //     key.classList.remove('playing');
 //   }, 100);
 
 const keys = Array.from(document.querySelectorAll(".key"));
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+
+  // Add click & touch support
+  key.addEventListener("click", handleClickOrTouch);
+  key.addEventListener("touchstart", handleClickOrTouch);
+});
 window.addEventListener("keydown", playsound);
+
+
+
+
